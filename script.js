dc2dd55ef2cc523e8d0dc181715729fafc15651b
scotts <juveduke@gmail.com>
2018-06-28 09:34:36 -0400

    Updates

--- script.js
+++ script.js
@@ -8,29 +8,47 @@ function getSize() {
 	return [x, y];
 }
 
-var tween;
+var NumImgs = 3;
 
+var tween;
+var imgs = [];
+
+for (i = 0; i < NumImgs; ++i) {
+  var img = document.createElement('img');
+  img.src = 'qrs/' + i + '.gif';
+  img.className = 'qr';
+  imgs.push(img);
+}
+
+var bodyEl = document.querySelector('body');
+console.log(bodyEl);
 window.onload = function start() {
+  for (i = 0; i < imgs.length; ++i) {
+    var img = imgs[i];
+    console.log(img);
+    bodyEl.appendChild(img);
+  }
+
 	window.setInterval(function () {
 		if (!tween || !tween.isActive()) {
-			tween = TweenMax.to("qr", 0.575, {
+			tween = TweenMax.to(".qr", 2, {
 				x: function() {
-					return Math.random() * getSize()[0];
+					return Math.random() * (getSize()[0] - 400);
 				},
 				y: function() {
-					return Math.random() * getSize()[1];
+					return Math.random() * (getSize()[1] - 400);
 				},
+        width: function() {
+          return 50 + (Math.random() * 350);
+        },
+        opacity: function() {
+          return Math.random() * 1;
+        },
 				//yoyo: true,
 				ease: Power1.easeIn,
 				//repeat: -1,
-				//onComplete: function() { $(this).; }
+				onComplete: function() { $(this).; }
 			})
 		}
 	}, 50);
 }
-function qrs() {
-
-}
-//var tl = new TimelineMax();
-//tl.add(
-//);
